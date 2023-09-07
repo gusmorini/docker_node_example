6b2a2dcef18741a35e7000db84e0f343d43b964f
Gustavo Morini <gustavommh@gmail.com>
2023-09-07 20:49:23 -0300

    crud server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,40 @@
 import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
 
 const app = fastify()
 const port = 3000
+const database = new DatabaseMemory()
 
-app.get('/', () => {
-  return 'Hello world'
+app.get('/videos', (request) => {
+  const { search } = request.query
+  const videos = database.list(search)
+  return videos
 })
 
-app.get('/fix', () => {
-  return 'Hello FIX'
+app.post('/videos', (request, reply) => {
+  const { title, description, duration } = request.body
+  database.create({ title, description, duration })
+  return reply.status(201).send()
+})
+
+app.put('/videos/:id', (request, reply) => {
+  const { id } = request.params
+  const { title, description, duration } = request.body
+
+  database.update(id, {
+    title,
+    description,
+    duration
+  })
+
+  return reply.status(204).send()
+
+})
+
+app.delete('/videos/:id', (request, reply) => {
+  const { id } = request.params
+  database.delete(id)
+  return reply.status(204).send()
 })
 
 app.listen({ port: port, host: '0.0.0.0' })
